dba163ff68db0996ff7313474ecaa61729ba7364
Viren Patadiya 073 <102893049+VIREN2779@users.noreply.github.com>
2025-09-16 10:23:32 +0000

    fix(page.tsx):file path change

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Link from "next/link";
-import AllProducts from "./components/add-product";
+import AllProducts from "./components/all-product";
 
 const Home = () => {
   return (
