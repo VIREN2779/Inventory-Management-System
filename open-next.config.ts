0f8ba2d18ab79d3197b72ad8e916b628c20f9663
Viren Patadiya <102893049+VIREN2779@users.noreply.github.com>
2026-03-05 06:11:57 +0000

    refactor(open-next.config.ts): simplify configuration export structure

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,4 @@
-import type { OpenNextConfig } from "@opennextjs/cloudflare";
-
-const config: OpenNextConfig = {
+export default {
   default: {
     override: {
       wrapper: "cloudflare-node",
@@ -12,5 +10,3 @@ const config: OpenNextConfig = {
     },
   },
 };
-
-export default config;
